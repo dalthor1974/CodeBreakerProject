2c90b85f8282b16f9f9ad04a32167d892454b517
Dimitrios Danellakis <dalthor1974@yahoo.gr>
2017-12-12 08:42:55 +0200

    Have built tasks 5-11

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,7 +3,32 @@ let attempt = document.getElementById('attempt');
 
 function guess() {
     let input = document.getElementById('user-guess');
-    //add functionality to guess function here
+    if (answer.value == '' || attempt.value == '') setHiddenFields();
+    if (!validateInput(input.value)) {
+      return false;
+    } else {
+      attempt.value++;
+    }
 }
 
-//implement new functions here
+function setHiddenFields() {
+  let number = Math.floor(Math.random() * 10000).toString();
+  while (number.length < 4) {
+    number = '0' + number;
+  }
+  answer.value = number;
+  attempt.value = 0;
+}
+
+function setMessage(message) {
+  document.getElementById('message').innerHTML = message;
+}
+
+function validateInput(number) {
+  if (number.length == 4) {
+    return true;
+  } else {
+    setMessage('Guesses must be exactly 4 characters long.');
+    return false;
+  }
+}
